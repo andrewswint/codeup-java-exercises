575a2ad23dfa4cbe4ea3304dfa28cfa4b8f93ac2
andrewswint <andrewswint1976@gmail.com>
2019-11-20 13:48:31 -0600

    exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,41 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 3;
+        String myString = "My favorite number";
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        float myNumber = 3.14f;
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+        int three = (int) "three";
+
+        int x = 4;
+        x+=5;
+
+        int x = 3;
+        int y = 4;
+        y+=x;
+
+          int x = 10;
+          int y = 2;
+          y-=x;
+          x/=y;
+
+
+        System.out.println(y);
+        byte number = 127+2;
     }
 }
