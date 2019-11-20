8aad20ee6c5b25ec59de37cfcf66c1506129f1cf
andrewswint <andrewswint1976@gmail.com>
2019-11-20 15:09:13 -0600

    done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,30 +12,30 @@ public class HelloWorld {
         System.out.println(x++);
         System.out.println(x);
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 
         String theNumberThree = "three";
         Object o = theNumberThree;
         int three = (int) o;
 
-        int three = (int) "three";
+//        int three = (int) "three";
 
-        int x = 4;
-        x+=5;
+//        int x = 4;
+//        x+=5;
+//
+//        int x = 3;
+//        int y = 4;
+//        y+=x;
+//
+//          int x = 10;
+//          int y = 2;
+//          y-=x;
+//          x/=y;
 
-        int x = 3;
-        int y = 4;
-        y+=x;
 
-          int x = 10;
-          int y = 2;
-          y-=x;
-          x/=y;
-
-
-        System.out.println(y);
-        byte number = 127+2;
+//        System.out.println(y);
+//        byte number = 127+2;
     }
 }
