7f607d714608264a12edc57082760947e477e612
andrewswint <andrewswint1976@gmail.com>
2019-11-22 11:08:04 -0600

    working on methods

--- src/Bob.java
+++ src/Bob.java
@@ -5,18 +5,6 @@ public class Bob {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Talk to Bob");
         String userInput = scanner.nextLine();
-//        do {
-//            System.out.println("Sure.");
-//        } while (userInput.endsWith("?"));
-//        do {
-//            System.out.println("Whoa, chill out!");
-//        } while (userInput.equals(userInput.toUpperCase()));
-//        do {
-//            System.out.println("Fine. Be that way!");
-//        } while (userInput.equals(""));
-//        do {
-//            System.out.println("Whatever.");
-//        } while (userInput.equals(userInput));
 
         do {
             if (userInput.endsWith("?")) {
@@ -28,7 +16,6 @@ public class Bob {
             } else {
                 System.out.println("Whatever.");
             }
-            System.out.println();
             userInput = scanner.nextLine();
         } while (!userInput.equals("goodbye"));
     }
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,5 +1,22 @@
 public class MethodsExercises {
-    public static void main (String[] args) {
+    public static int addition(int one, int two) {
+        return (one + two);
+    }
+    public static int subtraction (int one, int two) {
+        return (one - two);
+    }
+    public static int multiplication (int one, int two) {
+        return (one * two);
+    }
+    public static int division (int one, int two) {
+        return (one / two);
+    }
 
+
+    public static void main (String[] args) {
+        System.out.println(addition(1,1));
+        System.out.println(subtraction(2,1));
+        System.out.println(multiplication(2,2));
+        System.out.println(division(4,2));
     }
 }
