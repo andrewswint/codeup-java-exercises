5ab4c75579da0ef6bc003fdaa6ba6245530e0018
andrewswint <andrewswint1976@gmail.com>
2019-11-26 12:02:53 -0600

    finished problem two of exercise

--- src/Person.java
+++ src/Person.java
@@ -20,12 +20,26 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person = new Person("Andrew");
-        person.sayHello();
-        person.setName("Jacob");
-        System.out.println(person.name);
-        System.out.println(person.getName());
-        System.out.println(person.name);
+//        Person person = new Person("Andrew");
+//        person.sayHello();
+//        person.setName("Jacob");
+//        System.out.println(person.name);
+//        System.out.println(person.getName());
+//        System.out.println(person.name);
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1.getName() == person2.getName());
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
     }
 
 
