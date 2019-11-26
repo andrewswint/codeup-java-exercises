9f259233f7fa515188dfcbf25c4abc42b3bd0ef6
andrewswint <andrewswint1976@gmail.com>
2019-11-26 13:49:57 -0600

    problem three done

--- src/newPerson.java
+++ src/newPerson.java
@@ -2,5 +2,6 @@ public class newPerson {
     public static void main(String[] args) {
         Person person = new Person("Andrew");
         System.out.println(person.getName());
+
     }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,6 @@
 package util;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Input {
@@ -15,7 +17,6 @@ public class Input {
 
     public int getInt (int min, int max) {
         System.out.println("Please enter a number between 1 and 10");
-        Scanner sc = new Scanner(System.in);
         String newString = "Great! Your number is in range!";
         int newInput = sc.nextInt();
         do {
@@ -27,6 +28,46 @@ public class Input {
             } newInput = sc.nextInt();
         } while (newInput > max || newInput < min);
         return newInput;
+
+    }
+
+    public int getInt() {
+        System.out.println("Please enter a number between 1 and 10");
+        int newInput = sc.nextInt();
+        System.out.println(newInput);
+        return newInput;
+    }
+
+    public double getDouble (double min, double max) {
+        System.out.println("Please enter a number between 1 and 2");
+        String newString = "Great! Your number is in range!";
+        double newInput = sc.nextDouble();
+        do {
+            if (newInput < max && newInput > min) {
+                System.out.println("Great! " + newInput + " is in range!");
+                continue;
+            } else {
+                System.out.println("Do better. Please enter a number between 1 and 10");
+            } newInput = sc.nextDouble();
+        } while (newInput > max || newInput < min);
+        return newInput;
+    }
+
+    public double getDouble() {
+        System.out.println("Please enter a number between 1 and 10");
+        double newInput = sc.nextDouble();
+        System.out.println(newInput);
+        return newInput;
+    }
+
+
+    public static void main(String[] args) {
+        Input input = new Input();
+//        input.getInt(1,10);
+//        input.getInt();
+//        input.getDouble(1, 2);
+//        input.getDouble();
+
     }
 
 }
