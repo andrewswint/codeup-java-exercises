ef7d595330c4edd9bc2c9c24efb4a95fe869af22
andrewswint <andrewswint1976@gmail.com>
2019-11-29 23:56:45 -0600

    basic variable and method practice

--- src/WorkSpace/Practice.java
+++ src/WorkSpace/Practice.java
@@ -3,7 +3,9 @@ package WorkSpace;
 public class Practice {
 
     public static void main (String[] args) {
-
+        int myAge = 31;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 
 }
