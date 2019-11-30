02f01a974c112fc5e13d9f288deb332f49bb9737
andrewswint <andrewswint1976@gmail.com>
2019-11-29 23:20:34 -0600

    starting java video tutorial for practice

--- src/WorkSpace/Practice.java
+++ src/WorkSpace/Practice.java
@@ -3,27 +3,8 @@ import java.util.Scanner;
 
 class Practice {
 
-    private Scanner scan = new Scanner(System.in);
-    public String getName () {
-        System.out.println("Please enter authors name...");
-        String authorInput = scan.next();
-        System.out.println(authorInput);
-        return authorInput;
+    void sendEmail() {
+
     }
-
-    public String getEmail () {
-        System.out.println("enter email...");
-        String authorEmail = scan.next();
-        System.out.println(authorEmail);
-        return authorEmail;
-    }
-
-//    public String book () {
-//        getName();
-//        getEmail();
-//        return book();
-//    }
-
-
 
 }
--- src/WorkSpace/PracticeImplementaion.java
+++ src/WorkSpace/PracticeImplementaion.java
@@ -2,9 +2,6 @@ package WorkSpace;
 
 public class PracticeImplementaion {
         public static void main(String[] args) {
-            Practice practice = new Practice();
-            practice.getName();
-            practice.getEmail();
-//            practice.book();
+
         }
 }
