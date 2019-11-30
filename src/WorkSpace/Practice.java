49892c7317a0e2e64005e75fd624329db4e8a808
andrewswint <andrewswint1976@gmail.com>
2019-11-30 12:25:12 -0600

    mortgage calculator eercise complete

--- src/WorkSpace/Practice.java
+++ src/WorkSpace/Practice.java
@@ -1,14 +1,25 @@
 package WorkSpace;
 
-import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Practice {
 
     public static void main (String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point2.x = 2;
-        System.out.println(point1);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = sc.nextInt();
+        System.out.print("Annual percentage rate: ");
+        double annualPercent = sc.nextDouble();
+        System.out.print("Term (years): ");
+        int term = sc.nextInt();
+        double termMonths = term * (double) 12;
+        double percent = (annualPercent / (double)100);
+        double monthlyPercent = (percent / (double) 12);
+        double mortgage = principal * (monthlyPercent * Math.pow((1 + monthlyPercent),termMonths)) / (Math.pow((1 + monthlyPercent), termMonths) - 1);
+        NumberFormat monthlyPayment = NumberFormat.getCurrencyInstance();
+        String result = monthlyPayment.format(mortgage);
+        System.out.println("Your mortgage is: " + result);
     }
 
 }
