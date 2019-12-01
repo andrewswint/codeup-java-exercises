f64c3af06af51ca76adfc233d735efe81e0ac763
andrewswint <andrewswint1976@gmail.com>
2019-11-30 20:12:38 -0600

    OOP pracitce making objjects

--- src/OOP_Practice/Objects.java
+++ src/OOP_Practice/Objects.java
@@ -2,4 +2,15 @@ package OOP_Practice;
 
 public class Objects {
 
+     String cupType = "mug";
+     String cupColor = "black";
+     static boolean cupFull = false;
+     public static void fill() {
+        cupFull = true;
+    }
+    public static void empty() {
+         cupFull = false;
+    }
+
+
 }
--- src/OOP_Practice/Objects_Main.java
+++ src/OOP_Practice/Objects_Main.java
@@ -1,4 +1,15 @@
 package OOP_Practice;
 
 public class Objects_Main {
+
+    public static void main (String[] args) {
+        Objects cup = new Objects();
+        System.out.println(cup.cupColor);
+        System.out.println(cup.cupType);
+        System.out.println(cup.cupFull);
+        cup.fill();
+        System.out.println(cup.cupFull);
+
+    }
+
 }
