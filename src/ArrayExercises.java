1bc4152ae6a1c41d292ac8a45dc58491c39523ee
andrewswint <andrewswint1976@gmail.com>
2019-12-01 21:06:09 -0600

    working on the array eercise. finished creating the objects but struggling with the iteration

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -2,7 +2,27 @@ public class ArrayExercises {
 
 
     public static void main (String[] args) {
+
+        // Creation of person objects
+        Person person1 = new Person("Andrew");
+        person1.relationship = "Self";
+        Person person2 = new Person("Angela");
+        person2.relationship = "wife";
+        Person person3 = new Person("Mike");
+        person3.relationship = "friend";
+
+        // Creation of array of persons
         Person obj[] = new Person[3];
+        obj[0] = person1;
+        obj[1] = person2;
+        obj[2] = person3;
+
+        // Iteration through array of persons to print names
+
+        // not working!!!
+        for (int i = 0; i < obj.length; i++) {
+            System.out.println(obj[i].getName());
+        }
 
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,33 +1,30 @@
 public class Person {
 
-
-
-
-
-
-
+    public String relationship;
     private String name;
 
 // constructor
     public Person(String firstName) {
         this.name = firstName;
     }
+
+
 //
     public String getName(){
         return this.name;
     }
 //
-//    public void setName (String name) {
-//        this.name = name;
-//    }
-//
-    public void sayHello () {
-        System.out.println(this.name + " says hello!");
+    public void setName (String name) {
+        this.name = name;
     }
+//
+//    public void sayHello () {
+//        System.out.println(this.name + " says hello!");
+//    }
 
 //    public static void main(String[] args) {
         Person person = new Person("Andrew");
-        person.sayHello();
+//        person.sayHello();
 //        person.setName("Jacob");
 //        System.out.println(person.name);
 //        System.out.println(person.getName());
